ab8c976f6869d98ee92cd23a8aca9e50186257be
AkiraFukushima <h3.poteto@gmail.com>
2023-05-03 23:23:49 +0900

    Setup react-bootstrap

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,25 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/x-icon" href="/favicon/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="144x144"
+          href="/favicon/144x144.png"
+        />
+        <meta
+          name="description"
+          content="Single-column Fediverse client for desktop"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
