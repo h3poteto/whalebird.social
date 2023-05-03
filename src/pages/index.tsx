63f87a59609b5730e9679f20a7d1130a03ecfa1f
AkiraFukushima <h3.poteto@gmail.com>
2023-05-04 00:20:21 +0900

    Add privacy policy page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,12 @@ import Head from "next/head";
 import { Quicksand } from "@next/font/google";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
-import { Button, Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 import styles from "@/styles/Home.module.css";
+import Header from "@/components/Header";
+import { InferGetStaticPropsType } from "next";
+import Link from "next/link";
 
 const inter = Quicksand({ subsets: ["latin"] });
 
@@ -29,33 +32,7 @@ export default function Home() {
         <title>{t("title")}</title>
       </Head>
       <main className={inter.className}>
-        <Navbar bg="dark" variant="dark">
-          <Container>
-            <Navbar.Brand href="#">{t("title")}</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link href="/downloads">{t("menu.downloads")}</Nav.Link>
-              <Nav.Link href="/contacts">{t("menu.contacts")}</Nav.Link>
-              <Nav.Link href="/privacy_policy">
-                {t("menu.privacy_policy")}
-              </Nav.Link>
-              <Nav.Link href="https://github.com/h3poteto/whalebird-desktop">
-                {t("menu.repository")}
-              </Nav.Link>
-              <NavDropdown
-                title={t("menu.language.title")}
-                id="language-dropdown"
-              >
-                <NavDropdown.Item href="/en">
-                  {t("menu.language.english")}
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/ja">
-                  {t("menu.language.japanese")}
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Container>
-        </Navbar>
-
+        <Header />
         <div className={styles.main}>
           <h1>{t("title")}</h1>
           <h4 style={{ padding: "2vh" }}>{t("index.description")}</h4>
