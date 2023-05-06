2558a2a2bf62d1412e832d075613524b4be83e85
AkiraFukushima <h3.poteto@gmail.com>
2023-05-06 12:30:35 +0900

    Wrap SSRProvider for bootstrap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
+import { SSRProvider } from 'react-bootstrap'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <GoogleReCaptchaProvider reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!} language="en">
-      <Component {...pageProps} />
+      <SSRProvider>
+        <Component {...pageProps} />
+      </SSRProvider>
     </GoogleReCaptchaProvider>
   )
 }
